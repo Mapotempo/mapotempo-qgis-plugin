4b3b5b7dc3b1acbece84544a41fcbe33b5bec341
DariusM <dariusmatboo@gmail.com>
2015-07-23 12:03:18 +0200

    path issues

--- __init__.py
+++ __init__.py
@@ -21,7 +21,7 @@
  ***************************************************************************/
 """
 
-
+import os
 # noinspection PyPep8Naming
 def classFactory(iface):  # pylint: disable=invalid-name
     """Load PluginMapotempo class from file PluginMapotempo.
@@ -30,11 +30,25 @@ def classFactory(iface):  # pylint: disable=invalid-name
     :type iface: QgsInterface
     """
     #
+    # import sys
+    # sys.path.append('/home/darius/.qgis2/python/plugins/mapotempo-qgis-plugin/')
+
+    import sys
+    path = resolve('')
+    if not path in sys.path:
+        sys.path.append(path)
     from .plugin_mapotempo import PluginMapotempo
     from .plugin_mapotempo_layer import PluginMapotempoLayer
     from .plugin_mapotempo_handle import PluginMapotempoHandle
+
+
     __all__ = [
-       'SwaggerPetstore', 'apis', 'polyline', 'geojson'
+       'SwaggerPetstore', 'apis', 'geojson', 'polyline'
        ]
 
     return PluginMapotempo(iface)
+
+def resolve(name, basepath=None):
+    if not basepath:
+        basepath = os.path.dirname(os.path.realpath(__file__))
+    return os.path.join(basepath, name)
